045e104ea407fdef66c9caa75b4174ac2a0c568d
Jackson Henry <jackson.henry3@gmail.com>
2014-06-03 06:52:55 -0400

    cleaned up

--- filters.js
+++ filters.js
@@ -1,57 +1,21 @@
+var video        = document.querySelector("#vid"),
+canvas           = document.querySelector('#liveFeed'),
+	w              = canvas.width,
+	h              = canvas.height,
+	ctx            = canvas.getContext('2d'),
 
-    var video = document.querySelector("#vid");
-    var canvas = document.querySelector('#liveFeed');
-    var ctx = canvas.getContext('2d');
-    var localMediaStream = null;
-    var k = 0
-    var onCameraFail = function (e) {
-        console.log('Camera did not work.', e);
-    };
+function onCameraFail() {
+	ctx.textAlign = "center";
+	ctx.font      = "20px Georgia";
+	ctx.fillText("Camera did not work.",w/2,h/2);
+};
 
-    function snapshot() {
-        if (localMediaStream) {
-            ctx.drawImage(video, 0, 0);
-        }
-        // filterCanvas(grayscale)
-    }
-    setInterval(snapshot, 50 );
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-
-    window.URL = window.URL || window.webkitURL;
-
-    navigator.getUserMedia({video:true}, function (stream) {
-        video.src = window.URL.createObjectURL(stream);
-        localMediaStream = stream;
-    }, onCameraFail);
-
-
-          // apply a filter to the image data contained in the canvas object
-      function filterCanvas(filter) {
-        if (canvas.width > 0 && canvas.height > 0) {
-          var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-          filter(imageData);
-          ctx.putImageData(imageData, 0, 0);
-        }
-      }
-
-        grayscale = function (pixels, args) {
-    var d = pixels.data;
-        for (var i = 0; i < d.length; i += 4) {
-          var r = d[i];
-          var g = d[i + 1];
-          var b = d[i + 2];
-          k = document.getElementById('cutoff').value
-          if (r+g+b/3<k)
-          {
-            d[i] = d[i + 1] = d[i + 2] = 0;
-          };
-          if (r+g+b/3>k)
-          {
-            d[i] = d[i + 1] = d[i + 2] = 255;
-          };
-        }
-    return pixels;
-  };
-
-  filterCanvas(grayscale)
-   
+function streamVideoToCanvas(stream) {
+	// 
+	video.src = window.URL.createObjectURL(stream);
+	ctx.drawImage(video, 0, 0);
+}
+n = navigator
+n.getUserMedia = n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia || n.msGetUserMedia;
+window.URL = window.URL || window.webkitURL;
+n.getUserMedia({video:true}, streamVideoToCanvas(stream), onCameraFail);
