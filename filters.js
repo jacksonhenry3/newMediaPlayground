83b945a597dc09744f81938cf291c1d65f2113f1
Jackson Henry <jackson.henry3@gmail.com>
2014-06-03 13:17:14 -0400

    sdsd

--- filters.js
+++ filters.js
@@ -19,8 +19,8 @@ function videoToCanvas() {
 	}
 	modN = document.getElementById('modNumber').value
 	filterCanvas(f)
-	ctx.fillStyle = "#FF0000";
-		ctx.fillRect(0,0,150,75);
+	// ctx.fillStyle = "#FF0000";
+	// 	ctx.fillRect(0,0,150,75);
 }
 
 n = navigator
