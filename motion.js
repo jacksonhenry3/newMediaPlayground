b549c94a90c6b536c406751773e22ba33612b363
Jackson Henry <jackson.henry3@gmail.com>
2014-06-04 13:11:04 -0400

    color

--- motion.js
+++ motion.js
@@ -90,7 +90,7 @@ squared = function (pixels, args) {
       d[i+2] =Math.pow(b/255,power)*255
       if (d[i]+d[i+1]+d[i+2] != 255*3)
       {
-        d[i]=d[i+1]=d[i+2]=0
+        d[i]=d[i+1]=d[i+2]=255
       }
       else
       {
