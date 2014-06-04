4b57133783b01ec4622f81d65b8b6294e339ef56
Jackson Henry <jackson.henry3@gmail.com>
2014-06-04 11:53:57 -0400

    blacked out everything but light in squared

--- filters.js
+++ filters.js
@@ -190,7 +190,10 @@ squared = function (pixels, args) {
       d[i] =Math.pow(r/255,power)*255
       d[i+1] =Math.pow(g/255,power)*255
       d[i+2] =Math.pow(b/255,power)*255
-
+      if (d[i]+d[i+1]+d[i+2] != 255*3)
+      {
+        d[i]=d[i+1]=d[i+2]=0
+      }
     }
 
 		
